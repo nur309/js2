3f927277172a217f670d91c4e0a0782ca5567f35
MD NURNABI ISLAM <110608040+nur309@users.noreply.github.com>
2023-09-13 17:45:51 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,61 @@
+// text-change
+
+function textChange(){
+
+	document.getElementById('h2').innerHTML="Hello world"
+}
+
+
+// text-change
+
+function text(){
+	document.getElementById('h3').innerHTML="Hi Everyone"
+}
+
+// text-change
+
+function textRepeat(){
+	document.getElementById('h4').innerHTML="How are you?"
+}
+
+// text-change
+
+function textAgain(){
+	document.getElementById('h5').innerHTML="where are you?"
+	}
+
+// text-change
+
+function textBack(){
+	document.getElementById('h6').innerHTML="where are you going?"
+	}
+
+// text-change
+
+function textRack(){
+	document.getElementById('h7').innerHTML="where are you from?"
+	}
+
+// text-change
+
+function textWack(){
+	document.getElementById('h8').innerHTML="what are you doing now?"
+	}
+
+// text-change
+
+function textXack(){
+	document.getElementById('h9').innerHTML="what is your name?"
+	}
+
+// text-change
+
+function textOack(){
+	document.getElementById('h10').innerHTML="what about your self?"
+	}
+
+// text-change
+
+function textMack(){
+	document.getElementById('h11').innerHTML="are you going to school?"
+	}
